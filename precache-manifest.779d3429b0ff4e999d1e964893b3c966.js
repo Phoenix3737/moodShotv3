7d9fa03ddb9e38ed627dbc19c8a363657aa0ceb6
mark groves <groves999@gmail.com>
2019-01-13 09:38:51 -0500

    Updates

---
+++ precache-manifest.779d3429b0ff4e999d1e964893b3c966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ec84322583203ed544f0535870c1e59",
+    "url": "/moodShotv3/static/media/facebook.3ec84322.png"
+  },
+  {
+    "revision": "1e25999d300c316d610fcc48163eb4d8",
+    "url": "/moodShotv3/static/media/bar-main.1e25999d.png"
+  },
+  {
+    "revision": "c70811a6854945acd64a587039867922",
+    "url": "/moodShotv3/static/media/bar-landing.c70811a6.png"
+  },
+  {
+    "revision": "c375d92dcb22fe8ccf95",
+    "url": "/moodShotv3/static/js/runtime~main.c375d92d.js"
+  },
+  {
+    "revision": "b5f004a4d3705b54ed91",
+    "url": "/moodShotv3/static/js/main.b5f004a4.chunk.js"
+  },
+  {
+    "revision": "7bba0313d9b88518ac75",
+    "url": "/moodShotv3/static/js/1.7bba0313.chunk.js"
+  },
+  {
+    "revision": "b5f004a4d3705b54ed91",
+    "url": "/moodShotv3/static/css/main.08a551e7.chunk.css"
+  },
+  {
+    "revision": "ca060eaead861f43d2d1aad4b302f2d3",
+    "url": "/moodShotv3/index.html"
+  }
+];
